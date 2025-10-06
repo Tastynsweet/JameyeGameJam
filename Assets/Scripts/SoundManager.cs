0ba35bb07823c3ed9be49f2b69b3bd6584112845
Andy <ac744303@gmail.com>
2025-10-05 21:21:48 -0700

    Sound

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] AudioSource musicSource;
+
+    public AudioClip musicClip;
+
+    private void Start()
+    {
+        musicSource.clip = musicClip;
+        musicSource.Play();
+    }
+}
